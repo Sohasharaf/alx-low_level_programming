5e48236e957683a49bcbd316343792d934b5d369
Sohasharaf <bunny_eng@hotmail.com>
2023-07-13 07:02:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcomenis a piece of art,\n";
+	return (0);
+}
