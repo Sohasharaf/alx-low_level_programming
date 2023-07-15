ecd4152aebabaeb566025468dde673ac904fef05
Sohasharaf <bunny_eng@hotmail.com>
2023-07-14 19:22:42 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,16 @@ Auth: soha sharaf
 /**
  * main -  prints the alphabet in lowercase, and then in uppercase.
  *
- * Return: Always (0).
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	char letter:
-
 		for (letter = 'a' ; letter <= 'z' ; letter++)
 			putchar(letter);
-
 	for (letter = 'A' ; letter <= 'Z' ; letter++)
 		putchar(letter);
-
 	putchar('\n');
 
 	return (0);
