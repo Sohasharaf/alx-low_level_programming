56419e8a10876e5660db605176d3274e1fcec640
Sohasharaf <bunny_eng@hotmail.com>
2023-07-14 21:09:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: sohasharaf
+ */
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z' ; letter >= 'a' ; letter--)
+		putchar(letter);
+
+	putchar('\n')
+
+		return (0);
+}
