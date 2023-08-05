5cdfa374ce1f73cb6eeb95badc4f8b5de4ab1759
Sohasharaf <bunny_eng@hotmail.com>
2023-08-04 19:59:39 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,11 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
 
+	printf("%d\n", argc - 1);
 	return (0);
 }
